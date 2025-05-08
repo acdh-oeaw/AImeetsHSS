6fe65a99aafa9b41e68449a57f83928e54a937b3
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2025-05-08 10:32:32 +0200

    fix: internal links without trailling slash

--- src/lib/ensure-trailing-slash.ts
+++ src/lib/ensure-trailing-slash.ts
@@ -2,6 +2,6 @@ export function ensureTrailingSlash(pathname: string): string {
 	if (pathname.endsWith("/")) return pathname;
 
 	if (pathname.split("/").at(-1)?.includes(".")) return pathname;
-
+	if (pathname.split("/").at(-1)?.includes("#")) return pathname;
 	return `${pathname}/`;
 }
